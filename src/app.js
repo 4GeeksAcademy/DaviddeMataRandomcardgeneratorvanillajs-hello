a8fa0d3f81ad51b27285bca5fd5ef42911233a2b
davidmata97-cell <daviddemata97@gmail.com>
2025-01-05 18:22:50 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let first = "A ";
+
+  let palos = ["diamantes", "corazones", "picas", "treboles"];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+  let randomNumeros = Math.floor(Math.random() * numeros.length);
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let finalSuit = palos[randomPalos];
+
+  document.getElementById("cardContent").innerHTML = numeros[randomNumeros];
+  document.getElementById("theCard").className = "";
+  document.getElementById("theCard").classList.add("card");
+  document.getElementById("theCard").classList.add(getSuiteClass(finalSuit));
+
+  function getSuiteClass(palos) {
+    switch (palos) {
+      case "diamantes":
+        return "suit-diamantes";
+      case "picas":
+        return "suit-picas";
+      case "corazones":
+        return "suit-corazones";
+      case "treboles":
+        return "suit-treboles";
+    }
+  }
 };
